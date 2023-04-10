2236754a7008efffc06e5cdf7f224a2d0284345f
Kamal Chaudhari <kamalchaudhary502@gmail.com>
2023-04-10 09:23:43 +0530

    Added URL based access control for security

--- src/main/java/com/springboot/crud/security/SecurityConfig.java
+++ src/main/java/com/springboot/crud/security/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.springboot.crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -31,5 +34,26 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user1,user2,user3);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity security) throws Exception{
+
+        security.authorizeHttpRequests(configurer->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/service/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET,"/service/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST,"/service/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT,"/service/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE,"/service/employees/**").hasRole("ADMIN")
+                );
+
+
+        security.httpBasic();
+
+        security.csrf().disable();
+
+        return security.build();
+
     }
 }
